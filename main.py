40538ae0c3f561b006fd8cb66e2206abb94f52f9
shubhadipghosh <shubhadip.27@gmail.com>
2023-07-29 08:54:31 +0530

    version 1.2

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from  prettytable import  prettytable
 
+
 table = prettytable.PrettyTable()
+
 table.add_column("Pokemon", ["Pikachu","Squirtle","Charmander"] )
 table.add_column("Type", ["Electric","Water","Fire"] )
+table.align = 'l'
 print(table)
